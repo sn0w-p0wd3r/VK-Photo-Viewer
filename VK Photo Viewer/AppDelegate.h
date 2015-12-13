3d9a948d9156d3332cf4bdaec305571a6034af23
xrayman <xrayman64@gmail.com>
2015-12-13 20:56:18 +0200

    Initial Commit

---
+++ VK Photo Viewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VK Photo Viewer
+//
+//  Created by Andrii Sudoma on 12/13/15.
+//  Copyright © 2015 Andrii Sudoma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VK Photo Viewer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VK Photo Viewer
+//
+//  Created by Andrii Sudoma on 12/13/15.
+//  Copyright © 2015 Andrii Sudoma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
